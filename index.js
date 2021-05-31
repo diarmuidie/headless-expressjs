da4109a36afa67aabc2fc299fc57ff71a7341e1a
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2021-05-31 13:19:59 +0100

    Print headers

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+const port = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+  console.log('Atlas canary received a request');
+
+  var body = {
+    'status': 'OK',
+    'request': {
+      'headers': req.headers
+    }
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(body, null, 4));
+}).listen(port, () => {
+  console.log('Atlas canary listening on port: ', port);
+});
